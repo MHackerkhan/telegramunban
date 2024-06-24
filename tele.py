73ad0056777de797a3d54aa70eda1e09ac9c179c
MBKMADMAX <116323761+MHackerkhan@users.noreply.github.com>
2024-06-24 20:50:55 +0500

    Update tele.py

--- tele.py
+++ tele.py
@@ -1,5 +1,8 @@
 import sys
-
+import requests
+from colorama import init, Fore, Style
+import random
+import time
 # Check version
 PYTHON_VERSION = bytes([46]).decode().join(sys.version.split(bytes([32]).decode())[0].split(bytes([46]).decode())[:-1])
 if PYTHON_VERSION != bytes([51, 46, 57]).decode():
